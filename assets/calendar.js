c94fe01004624ca2b052aeb88c82ffa3bc5731db
Corey Post <corey.post0120@gmail.com>
2020-06-23 02:32:00 -0600

    started javascript

---
+++ assets/calendar.js
@@ -0,0 +1,5 @@
+//set up document.redy to make sure the html loads before the script
+$(document).ready(function(){
+    // call moment.js to set up current date in day of week, month, day of month
+    
+});
